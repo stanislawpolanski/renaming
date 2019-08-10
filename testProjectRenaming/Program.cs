f632dff60714c8739388c0e45f9b62ae5617486f
Staszek <stanislaw.polanski@hotmail.com>
2019-08-10 19:00:45 +0200

    Add project files.

---
+++ testProjectRenaming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testProjectRenaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
